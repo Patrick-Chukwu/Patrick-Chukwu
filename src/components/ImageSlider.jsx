c133468f6d0f2d8c4cad720ddc75788587c254d1
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-01 18:01:44 +0100

    Update file

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './ImageSlider.css';
+import project1 from '../assets/Projectts/project1.webp'
+import project2 from '../assets/Projectts/project2.webp'
+import project3 from '../assets/Projectts/project3.webp'
+import project4 from '../assets/Projectts/project4.webp'
+
+const images = [
+  project1,
+  project2,
+  project3,
+  project4
+];
+
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative w-[80%]  mx-auto h-[90%] overflow-hidden my-8 p-4 opacity-40 hover:opacity-90  focus:opacity-90">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Slide ${index}`}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-transform duration-1000 ease-in-out ${index === currentIndex ? 'transform translate-x-0 opacity-90' : 'transform -translate-x-90 opacity-10'}`}
+          style={{ left: `${index * 10}%`, zIndex: index === currentIndex ? 10 : 0 }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import ImageSlider from '../components/ImageSlider'
 
 const Home = () => {
   return (
     <div className='flex flex-col mt-20'>
         <Navbar/>
-        <section className="pl-4 h-dvh flex flex-col items-left justify-start">
+        <section className="pl-4 h-dvh flex flex-col items-left justify-start items-center">
           <h2 className='text-primary text-2xl'>Meet Patrick Chukwu</h2>
           <p className="text-secondary font-semibold text-3xl">Front-end Developer</p>
           <p className='text-primary text-2xl'>Building amazing stuff.</p>
+          <ImageSlider/>
 
 
         </section>
