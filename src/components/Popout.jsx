8d07d1a7f171bf5570c7b401482cfedd227583a0
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-03 21:00:25 +0100

    Update file

--- src/components/Popout.jsx
+++ src/components/Popout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { FaMailBulk } from 'react-icons/fa'
 
 const Popout = () => {
 
@@ -8,16 +9,16 @@ const item = { hidden: { x: -40, opacity: 10, scale: 1.2}}
   return (
     <div>
       <motion.div 
-      className=' w-fit bg-white p-4'
+      className=' w-fit bg-black p-2 rounded-full'
       animate={{x: 100, y: 300, rotate: 60}}
       whileHover={{scale: 1.5}}
       // whileTap={{x: -20, scaleX: 2}}
       drag="y"
       dragConstraints={{ top:-100, bottom: 100}}
-      >Popout</motion.div>
+      ><a href="mailto:chukwupatrickify@gmail.com" target='_blank'><FaMailBulk/></a></motion.div>
 
 
-      <p>New animation on list</p>
+      {/* <p>New animation on list</p> */}
       <motion.ul className='flex gap-4 w-fit mx-auto' animate="hidden" variants={list}>
         <motion.li variants={item} className='size-28 bg-white'/>
         <motion.li whileHover={{scale: 0.8, right: 100}} variants={item} className='size-28 bg-black'/>
