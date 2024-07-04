acd2436ee8cef455025e38ae471a640b99a9d834
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-04 12:44:57 +0100

    Update file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,19 +11,19 @@ const Home = () => {
   return (
     <div className='flex flex-col mt-24'>
         <Navbar/>
-        <section className="pl-4 h-dvh flex flex-col items-left justify-start items-center">
-          <div>
-          <h2 className='text-primary text-2xl'>Meet Patrick Chukwu</h2>
-          <p className="text-secondary font-semibold text-3xl">A Software Developer</p>
-          <p className='text-primary text-2xl'>Building amazing stuff.</p>
-          <p className='text-secondary text-sm'>I help businesses build amazing web applications and products that scales with great user experiences.</p>
+        <section className="p-2 h-dvh flex flex-col items-left justify-start items-center">
+          <div className=' pl-2'>
+          <h2 className='text-primary text-2xl text-left'>Meet Patrick Chukwu</h2>
+          <p className="text-secondary font-semibold text-3xl text-left">A Software Developer</p>
+          <p className='text-primary text-2xl text-left'>Building amazing stuff.</p>
+          <p className='text-secondary text-sm text-left'>I help businesses build amazing web applications and products that scales with great user experiences.</p>
 
           </div>
           
-          <div className="flex justify-between gap-8 w-[90%] items-center p-4 md:w-1/2 mx-auto my-4">
+          <div className="flex justify-between gap-8 w-[95%] items-center p-2 md:w-1/2 mx-auto my-4">
           <a href="https://calendar.app.google/zYzRKYgvBGGoMdw18" target='_blank' className='text-white bg-bcolor px-4 py-2 rounded-md hover:bg-white hover:text-bcolor font-medium border border-black'>Book a call</a>
          
-         <div className="flex justify-center gap-2">
+         <div className="flex justify-center items-center gap-2">
          <a href="https://github.com/Patrick-Chukwu" target='_blank' className='border-none rounded-full p-1 bg-primary text-black hover:text-black hover:bg-white'><FaGithub/></a>
          <a href="mailto:chukwupatrickify@gmail.com" target='_blank' className='border-none rounded-full p-1 bg-primary text-black hover:text-black hover:bg-white'><FaMailBulk/></a>
          </div>
