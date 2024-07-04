8af5ab2b0d1db82b79b3c393ae3bdbabe20978d2
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-04 11:59:17 +0100

    Update file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Skills from '../components/Skills'
 
 const About = () => {
   return (
@@ -8,20 +9,23 @@ const About = () => {
       <div className="flex flex-col items-center justify-start gap-4">
         <h2 className="text-3xl font-semibold w-[90%] max-w-[600px]">Hi, I am Patrick, <span className="text-2xl">a Software Engineer at the intersection of tech, social impact and education.</span>
         </h2>
-        <div className="flex flex-col md:flex-row gap-4 justify-between items-center max-w-[700px]">
-        <p className="text-xl font-medium p-4 text-left">
+  
+        <div className=" gap-4 justify-between items-center max-w-[700px]">
+        <p className="text-sm md:text-xl font-medium p-4 text-left">
 
-I am driven by a passion for leveraging technology to solve complex challenges. I thrive on delivering exceptional user experiences through web solutions. My expertise spans majorly front-end (#React, #Next.js and #Vue)  and lately back-end (#Node.js), ensuring seamless functionality and optimal performance. 
-
-I collaborate closely with clients to understand their unique requirements while bringing creativity and attention to detail to every project. I'm committed to staying at the forefront of industry trends and technologies by continuously enhancing my skills to deliver results that exceed expectations.
+I am driven by a passion for leveraging technology to solve complex challenges. I thrive on delivering web solutions with exceptional user experiences while ensuring seamless functionality and optimal performance.
 
 </p>
-<p className="p-4">When I'm not busy exploring new technologies, you'll find me contributing to courses that better humanity either through the social impact ventures I am a part of or paying it back through advocacy and actions that foster education.
+<p className="text-sm md:text-xl font-medium p-4 text-left">I collaborate closely with clients to understand their unique requirements while bringing creativity and attention to detail to every project. I'm committed to staying at the forefront of industry trends and technologies by continuously enhancing my skills to deliver results that exceed expectations.</p>
+<p className="p-4 text-sm font-medium md:text-xl">When I'm not busy exploring new technologies, you'll find me contributing to courses that better humanity either through the social impact ventures I am a part of or paying it back through advocacy and actions that foster education.
 
 They say curiosity killed the cat, but either I'm the cat that can't die or I'm here to prove them wrong!</p>
         </div>
    
       </div>
+
+      <h3 className="text-xl font-bold text-center text-secondary">Skills</h3>
+      <Skills/>
     </div>
   )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Skills from '../components/Skills'
-import Popout from '../components/Popout'
 import Projects from '../components/Projects'
 
 const Portfolio = () => {
   return (
     <div className='mt-24'>
       <Navbar/>
-      <h2 className="text-2xl text-secondary font-semibold text-center">Skills</h2>
-      <Skills/>
-      <Popout/>
+      <h2 className="text-3xl text-secondary  text-center font-bold">Portfolio</h2>
+      <p className='text-secondary text-sm p-4 text-center w-fit mx-auto'>Over the years, I have worked on amazing products and projects across several industry.</p>
+
       <Projects/>
     </div>
   )
