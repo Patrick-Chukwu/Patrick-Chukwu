c26c8e88f6c41d7908e366c89358c8a666c3841d
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-18 15:34:15 +0100

    feat: update file

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -4,12 +4,14 @@ import project1 from '../assets/Projectts/project1.webp'
 import project2 from '../assets/Projectts/project2.webp'
 import project3 from '../assets/Projectts/project3.webp'
 import project4 from '../assets/Projectts/project4.webp'
+import project5 from '../assets/Projectts/timbucloud.webp'
 
 const images = [
   project1,
   project2,
   project3,
-  project4
+  project4,
+  project5
 ];
 
 const ImageSlider = () => {
@@ -29,7 +31,7 @@ const ImageSlider = () => {
           key={index}
           src={image}
           alt={`Project ${index}`}
-          className={`absolute top-0 left-0 w-full h-full object-cover transition-transform duration-1000 ease-in-out ${index === currentIndex ? 'transform translate-x-0 opacity-90' : 'transform -translate-x-90 opacity-10'}`}
+          className={`absolute top-0 left-0 w-full h-full object-contain transition-transform duration-1000 ease-in-out ${index === currentIndex ? 'transform translate-x-0 opacity-90' : 'transform -translate-x-90 opacity-10'}`}
           style={{ left: `${index * 5}%`, right: `${(index + 1) * 20}`, zIndex: index === currentIndex ? 10 : 0 }}
         />
       ))}
